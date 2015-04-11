df0a18902d4a94df5fb0833f9b9fb2918ee51ddb
Julia <juli@isdigit.al>
2015-04-11 13:36:23 -0600

    More updates/changes for Bootstrap/Mock-up site

--- html_week_2/bootstrap/scripts.js
+++ html_week_2/bootstrap/scripts.js
@@ -1,1 +1,30 @@
 // This is a comment at the top of the script.
+//sticky nav
+
+//grab the <p> elements with quotes
+
+var quotes=$('.quotes'); //.quotes references the <p> class of quotes
+
+// quotes.length counts the number <p> element with class .quotes. if the length is greater than 1, then run the function, showQuotes
+    if(quotes.length > 1) {
+        //showQuote();
+        $(quotes[0]).css('display', 'block');
+        setInterval(function() {timedQuotes()},4000);
+    }
+    function showQuote() {
+
+        //alert("hello");
+        var random=Math.floor(Math.random()*quotes.length);
+        //console.log(random);
+        //$(quotes[random]).css("display", "block").css("color", "red"); called chaining
+        $(quotes[random]).css("display", "block");
+    }
+    function timedQuotes(){
+        quotes.css('display', 'none');
+        var random=Math.floor(Math.random()*quotes.length);
+        console.log(random);
+        $(quotes[random]).css("display", "block")
+
+    }
+
+//console.log(quotes);
